4a87285e89bf65162706e6c47314317fd5c7581e
resl <resl@knarr.robotlab>
2016-04-06 12:08:03 -0700

    Added popup to video plugin

--- rowi_plugins/plugins/video/html/plugin.js
+++ rowi_plugins/plugins/video/html/plugin.js
@@ -22,12 +22,39 @@ VideoThumbnailPlugin.prototype.init = function() {
     this.panel = ROWI.add_panel(this, 'Video', "/"+this.namespace+"/video");
 
     this.container = $('#video_container');
+    this.topic = this.config.topic;
 
     $('#video_panel').appendTo(this.panel);
 
     $('#video_toggle_button').click(this.toggle_video.bind(this));
+    $('#video_popup_button').click(this.popup_video.bind(this));
+    this.topic_select = $('#video_topic_select');
+    $('#video_topic_select').change(this.topic_select_callback.bind(this));
+
+    ROWI.ros.getTopicsForType('sensor_msgs/Image', this.topics_callback.bind(this));
+
     this.update_snapshot();
     this.timer = setInterval(this.update_snapshot.bind(this),2000);
+};
+
+VideoThumbnailPlugin.prototype.topics_callback = function(data) {
+  $.each(data, (function(key, val) {
+	$('<option>', { value: val }).appendTo(this.topic_select).text(val);
+  }).bind(this));
+};
+
+VideoThumbnailPlugin.prototype.topic_select_callback = function(data) {
+	this.topic = this.topic_select.val();
+        this.update_video();
+};
+
+
+VideoThumbnailPlugin.prototype.popup_video = function() {
+    if(this.playing) {
+	this.toggle_video();
+    }
+
+    window.open(this.config.web_video_server_url+'stream_viewer?topic='+this.topic, "Video", "width=640,height=500");
 };
 
 VideoThumbnailPlugin.prototype.toggle_video = function() {
@@ -36,19 +63,29 @@ VideoThumbnailPlugin.prototype.toggle_video = function() {
     this.container.attr('src','');
     this.playing = false;
   } else {
-    this.container.attr('src',this.config.web_video_server_url+'stream?topic='+this.config.topic);
     this.playing = true;
+    this.update_video();
   }
   this.update_button();
 }
 
+VideoThumbnailPlugin.prototype.update_video = function() {
+    if(!this.playing) {
+	return;
+    }
+    this.container.attr('src',this.config.web_video_server_url+'stream?topic='+this.topic);
+}
+
 VideoThumbnailPlugin.prototype.update_snapshot = function() {
   if(!$(this.panel).is(":visible")) {
+    if(this.playing) {
+	this.toggle_video();
+    }
     return;
   }
 
   if(!this.playing) {
-    this.container.attr('src',this.config.web_video_server_url+'snapshot?topic='+this.config.topic);
+    this.container.attr('src',this.config.web_video_server_url+'snapshot?topic='+this.topic);
   }
 }
 //
